d705be27ff717aa6f9cc420f1d25dfe354048630
Johnson Chen <chnejohnson@gmail.com>
2024-03-16 18:40:21 +0800

    app: GameHeader.vue

--- app/components/Game.client.vue
+++ app/components/Game.client.vue
@@ -256,63 +256,11 @@ function onClickBottomBtn() {
 }
 
 // 'text-pink-500' : 'text-blue-400'
-
-const showTips = ref(false)
 </script>
 
 <template>
 	<div>
-		<div class="p-3 pb-0 flex justify-between">
-			<div>
-				<div class="flex gap-1">
-					<div>Player 1: Thief</div>
-					<div></div>
-				</div>
-				<div class="flex gap-1">
-					<div>Player 2: Police</div>
-					<div>
-						<n-countdown
-							:render="
-								({ minutes, seconds }) => {
-									return [
-										h('span', [String(minutes).padStart(2, '0')]),
-										':',
-										h('span', [String(seconds).padStart(2, '0')]),
-									]
-								}
-							"
-							:duration="180000"
-							:precision="0"
-							:active="true"
-						/>
-					</div>
-				</div>
-			</div>
-
-			<div class="flex items-center gap-3">
-				<Icon
-					class="hover:text-teal-300 cursor-pointer"
-					size="24"
-					name="ic:baseline-library-books"
-					@click="showTips = true"
-				/>
-				<!-- <n-button size="tiny">Share</n-button> -->
-				<div class="flex items-center gap-1">
-					<Icon size="24" name="ic:round-groups" />
-					<p>2</p>
-				</div>
-			</div>
-
-			<n-drawer v-model:show="showTips" :height="200" placement="top">
-				<n-drawer-content title="Tips">
-					<ul class="list-disc px-5">
-						<li>
-							Each game consists of two rounds, with players taking turns being the thief and the police.
-						</li>
-					</ul>
-				</n-drawer-content>
-			</n-drawer>
-		</div>
+		<GameHeader />
 
 		<!-- Game in progress state -->
 		<div v-if="gameState === 1 || gameState === 2" class="game-state">
